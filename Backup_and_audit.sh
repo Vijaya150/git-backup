ff8b97729aae10ea02a0a3bc28b18c090490cbe2
Vijaya150 <vijayadarshini1503@gmail.com>
2025-06-14 13:50:50 +0530

    Update Backup_and_audit.sh

--- Backup_and_audit.sh
+++ Backup_and_audit.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
 
 GITREPO="gitrepolink.txt"
+BACKUP_DIR="/var/backups/git-repos"
 
-sudo mkdir -p /var/backups/git-repos/
-sudo chmod -R 770 /var/backups/git-repos/
+mkdir -p "$BACKUP_DIR"
+chmod -R 770 "$BACKUP_DIR"
 
 while read -r gitlink;do
-	if [ -z "$gitlink" ];then
-		echo "link is empty"
-	continue
-	fi
+        if [ -z "$gitlink" ];then
+                echo "link is empty"
+        continue
+        fi
 
-		repo_name=`(basename "$gitlink" .git)`
-	
-	if [ -d "$repo_name" ];then	
-		cd "$repo_name" 
-		git pull
-		cd ..
-		echo "Pulled latest changes"
-	else
-		git clone "$gitlink"
+                repo_name=`(basename "$gitlink" .git)`
+
+        if [ -d "$repo_name" ];then
+                cd "$repo_name" 
+                git pull
+                cd ..
+                echo "Pulled latest changes"
+        else
+                git clone "$gitlink"
                 echo "git cloned"
-	fi
+        fi
 
-sudo tar -czf /var/backups/git-repos/${repo_name}_$(date "+%Y%m%d").tar.gz "$repo_name"
+tar -czf ${BACKUP_DIR}/${repo_name}_$(date "+%Y%m%d").tar.gz "$repo_name"
 echo "tarball backup is done"
 
-	if [ -d "$repo_name/.git" ];then
-		cd "$repo_name" || continue
-		git log --since=1.day | sudo tee /var/backups/git-repos/audit_${repo_name}_$(date "+%Y%m%d").txt > /dev/null
+        if [ -d "$repo_name/.git" ];then
+                cd "$repo_name" || continue
+                git log --since=1.day > "${BACKUP_DIR}/audit_${repo_name}_$(date "+%Y%m%d").txt"
 
                echo "git logs collected"
-	       cd ..
+               cd ..
        else
-	       echo "Git repo not found"
-	fi
+               echo "Git repo not found"
+        fi
 
 done<"$GITREPO"
-
-
